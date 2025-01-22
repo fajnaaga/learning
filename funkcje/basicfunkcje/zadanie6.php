dfb89c0b920b0a276c2347b7dc6fb91e659b8759
fajnaaga <agatamaria.krajewska@gmail.com>
2025-01-22 17:34:43 +0100

    exercise6 php

---
+++ funkcje/basicfunkcje/zadanie6.php
@@ -0,0 +1,7 @@
+<?php
+// Create function "displayNameLength" that will take a $name as an argument and display the length of the string.
+// Stwórz funkcje "displayNameLength", która przyjmie argument $name i wyświetli długość stringa.
+function displayNameLength($name){
+echo strlen($name);
+};
+displayNameLength("Bartosz");
