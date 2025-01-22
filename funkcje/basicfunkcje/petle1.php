fb85395a85a071b10ad41fb05ba31a0eda555606
fajnaaga <agatamaria.krajewska@gmail.com>
2025-01-22 21:18:35 +0100

    petle1.php

--- funkcje/basicfunkcje/petle1.php
+++ funkcje/basicfunkcje/petle1.php
@@ -6,4 +6,16 @@ function showAllNumbers($max) {
         echo "The number is: $x<br>";
     }
 }
-showAllNumbers(7);
+// showAllNumbers(7);
+
+// Create function "showEvenNumbers" that accept 1 argument $max and display all even numbers from 0 to $max
+// Stwórz funkcję "showEvenNumbers" która przyjmuje 1 argument $max i wyświetla wszystkie parzyste liczby od 0 do $max
+
+function showEvenNumbers($max) {
+    for ($x = 0; $x <= $max; $x++) {
+           if ($x % 2 === 0) {
+        echo "$x <br>";
+        } 
+    }
+}
+showEvenNumbers(23);
