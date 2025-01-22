6eb0af851b23ed3f61d37eef5f0d8b8693de6fd0
fajnaaga <agatamaria.krajewska@gmail.com>
2025-01-22 17:05:05 +0100

    createsumNumbers

---
+++ funkcje/basicfunkcje/zadanie5.php
@@ -0,0 +1,9 @@
+<?php
+// Create function "sumNumbers" that takes two arguments $number1 and $number2 and returns the sum of those arguments
+// Stworz funckje "sumNumbers" ktora przyjmuje dwa argumenty $number1 i $number2 i zwraca sume tych argumentow
+
+function sumNumbers($number1, $number2) {
+return $number1 + $number2;
+}
+echo sumNumbers(7, 3);
+
