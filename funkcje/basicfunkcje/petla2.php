f11d8f96e8350ae153279da3ecc2f5b12d0bd52f
fajnaaga <agatamaria.krajewska@gmail.com>
2025-01-23 12:36:55 +0100

    petla2.php

---
+++ funkcje/basicfunkcje/petla2.php
@@ -0,0 +1,25 @@
+<?php
+// Create function "showNumbersToZero" that accept 1 argument $start and display all numbers from $start to 0
+// Stwórz funkcję "showNumbersToZero" która przyjmuje 1 argument $start i wyświetla wszystkie liczby od $start do 0
+// showNumbersToZero(5); // 5, 4, 3, 2, 1, 0
+// showNumbersToZero(10); // 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+
+// showNumbersToZero(0); // 0
+// showNumbersToZero(-5); // -5, -4, -3, -2, -1, 0
+
+function showNumbersToZero ($start) { 
+    if ($start >=0 ) {
+        for ($index = $start; $index >= 0; $index--) 
+        { 
+            echo $index . "<br>";
+        }
+    } else {
+        for($index = $start; $index <= 0; $index++)
+        {
+           echo $index . "<br>";
+        }
+    } 
+}
+showNumbersToZero(7) ;
+showNumbersToZero(0) ;
+showNumbersToZero(-7) ;
