2bba4dcfbd081d645b881e86caef2e6ca11f7f73
fajnaaga <agatamaria.krajewska@gmail.com>
2025-01-23 20:53:02 +0100

    array2.php

--- funkcje/basicfunkcje/array2.php
+++ funkcje/basicfunkcje/array2.php
@@ -5,22 +5,31 @@
 // Stwórz funkcję "showPersonDetails" która przyjmuje 1 argument $person i wyświetla imię, wiek i miasto osoby
 // Użyj tablic $person1 i $person2 z poprzedniego zadania :)
 
-function showPersonDetails($person) { 
-echo $person["name"]. "<br>" ;
-echo $person['age']. "<br>";
-echo $person['city']. "<br>";
+// Modify function "showPersonDetails" to do not display person values if this value do not exist
+// Zmodyfikuj funkcję "showPersonDetails" aby nie wyświetlała wartości osoby jeśli ta wartość nie istnieje
 
+function showPersonDetails($person) { 
+    if (array_key_exists('name',$person)) {
+        echo $person['name']. '<br>' ;
+    }
+    if (array_key_exists('age',$person)) {
+        echo $person['age']. '<br>';
+    }
+    if (array_key_exists('city',$person)) {
+        echo $person['city']. '<br>';
+    }
 }
 
 $person1 = [
-    "name" => "Franciszek",
-    "age" => "37",
-    "city" => "Londyn",
+    'name' => 'Franciszek',
+    'age' => '37',
+//   'city' => 'Londyn',  
   ];
 
 $person2 = [
-    "name" => "Faustyna",
-    "age" => "34",
-    "city" => "Gliwice",
+    'name' => 'Faustyna',
+    'age' => '34',
+    'city' => 'Gliwice',
 ];
 showPersonDetails($person1);
+
