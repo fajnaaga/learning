ae45693f9cc0d07dab6a1561ce71d3acc473b10d
fajnaaga <agatamaria.krajewska@gmail.com>
2025-01-23 21:28:40 +0100

    array3.php

---
+++ funkcje/basicfunkcje/array3.php
@@ -0,0 +1,8 @@
+<?php // 
+// Create function showArrayLength that accept 1 argument $array and display length of this array
+// Stwórz funkcję showArrayLength która przyjmuje 1 argument $array i wyświetla długość tej tablicy
+
+function showArrayLength($array) {  
+return count($array);
+}
+echo ShowArrayLength ([1,2,1,3]);
